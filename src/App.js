8bb5289f374f30dee42e75c4c344416fa986b5b5
OllaZel <olka0601@mail.ru>
2023-03-30 21:03:55 +0300

    fourCorrect

--- src/App.js
+++ src/App.js
@@ -14,19 +14,21 @@ import Home from './Home';
 function App() {
   return <Router>
     <nav className='menu'>
-      <Link className='link' to="/home">ГЛАВНАЯ</Link>
+      <Link className='link' to="/">ГЛАВНАЯ</Link>
       <Link className='link' to="/about">О НАС</Link>
       <Link className='link' to="/products">ПРОДУКЦИЯ</Link>
       <Link className='link' to="/contacts">КОНТАКТЫ</Link>
     </nav>
 
     <Routes>
-      <Route path="/home" element={<Home/>}/>
+      <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/products" element={<Products/>}/>
       <Route path="/contacts" element={<Contact/>}/>
     </Routes>
   </Router>;
+
+  
 
 
 }
